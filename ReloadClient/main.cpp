67c4d4d2ca3f264a39c1180976d733d27b793651
Mikael Kindborg <mikael.kindborg@mosync.com>
2012-11-22 10:35:26 +0100

    [RELOAD-124] ReloadClient: Removed LocalFiles.bin from version control. Updated a comment in main.cpp.

--- ReloadClient/main.cpp
+++ ReloadClient/main.cpp
@@ -28,8 +28,6 @@ MA 02110-1301, USA.
 
 /**
  * Main function that is called when the program starts.
- * Here an instance of the MyMoblet class is created and
- * the program enters the main event loop.
  */
 extern "C" int MAMain()
 {
