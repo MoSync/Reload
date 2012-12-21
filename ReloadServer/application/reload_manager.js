3fac9e180c0eb8ffcbacc2c33d12c6ef9a3928e5
Kostas Tsolakis <konstantinos.tsolakis@mobilesorcery.com>
2012-12-21 15:41:20 +0100

    RELOAD-137 Fixing bug that crashed server if a previous request was responded after another request

--- ReloadServer/application/reload_manager.js
+++ ReloadServer/application/reload_manager.js
@@ -86,7 +86,7 @@ var rpcFunctions = {
             });
 
             sendResponse({hasError: false, data: projectListJSON});
-        });
+        },sendResponse);
     },
 
     // internal function
