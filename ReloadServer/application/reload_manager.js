MoSync/Reload

commit 3fac9e180c0eb8ffcbacc2c33d12c6ef9a3928e5

RELOAD-137 Fixing bug that crashed server if a previous request was responded after another request

ReloadServer/application/reload_manager.js
@@ -86,7 +86,7 @@ var rpcFunctions = {
             });
 
             sendResponse({hasError: false, data: projectListJSON});
-        });
+        },sendResponse);
     },
 
     // internal function

ReloadServer/lib/jsonrpc.js
@@ -25,10 +25,11 @@ var JSONRPC = {
 	* @type void
 	*/  
 	listen : function(message, response){
-		var self = this;
-		this.response = response;
 
-		self.handleMessage(message, function(processingResult) {
+		/*var self = this;
+		this.response = response;*/
+
+		this.handleMessage(message, function(processingResult) {
 			
 			//console.log(processingResult);
 
@@ -47,27 +48,27 @@ var JSONRPC = {
 	      	
 	      	// using id attribute for sending response in binary format
 	      	if((message.id == 1) && (!processingResult.hasError)) {
-	      		self.response.writeHead(200, {
+	      		response.writeHead(200, {
 						  'Content-Length': processingResult.data.length,
 						  'Content-Type': 'binary',
 						  'Pragma': 'no-cache',
 						  'Cache-Control': 'no-cache',
 						  'Expires': '-1'
 						});
-				self.response.write( processingResult.data );
-				self.response.end("");
+				response.write( processingResult.data );
+				response.end("");
 	      	}
 	      	else {
 				//console.dlog("SENDING RESPONSE: " + JSON.stringify(responseObject));
-				self.response.writeHead(200, {
+				response.writeHead(200, {
 							  'Content-Length': JSON.stringify(responseObject).length,
 							  'Content-Type': 'application/json',
 							  'Pragma': 'no-cache',
 							  'Cache-Control': 'no-cache',
 							  'Expires': '-1'
 							});
-				self.response.write( JSON.stringify(responseObject) );
-				self.response.end("");
+				response.write( JSON.stringify(responseObject) );
+				response.end("");
 			}
 		});
 	},

commit 52cf0d0c64e4ed9d60ae18d9f4c48643416af684

RELOAD-137 Fixed bug that was causing server crash if there is double request for project deletion. Also fixing bug that was introduced after commit 3fac9e180c0eb8ffcbacc2c33d12c6ef9a3928e5 and crashed the server if there was not rpc function or there was wrong parameters.

ReloadServer/application/reload_manager.js
@@ -224,24 +224,27 @@ var rpcFunctions = {
 
     removeProject: function (projectName, sendResponse) {
 
+        var responseSent = false;
         //check if parameter passing was correct
-        if(typeof sendResponse !== 'function') return false;
+        if ((typeof sendResponse !== 'function') || projectName === "") {
+            return false;
+        }
 
         var projectPath = vars.globals.rootWorkspacePath +
                           vars.globals.fileSeparator +
                           projectName;
 
-        fs.removeRecursive = function (path,cb) {
+        removeRecursive = function (path, cb) {
             var self = this;
 
             fs.stat(path, function (err, stats) {
-                if(err) {
 
+                if (err) {
                     cb(err, stats);
                     return;
                 }
 
-                if(stats.isFile()) {
+                if (stats.isFile()) {
 
                     fs.unlink(path, function (err) {
                         if(err) {
@@ -253,13 +256,14 @@ var rpcFunctions = {
                         return;
                     });
                 }
-                else if(stats.isDirectory()) {
+                else if (stats.isDirectory()) {
                     // A folder may contain files
                     // We need to delete the files first
                     // When all are deleted we could delete the
                     // dir itself
                     fs.readdir(path, function (err, files) {
-                        if(err) {
+
+                        if (err) {
 
                             cb(err,null);
                             return;
@@ -275,10 +279,10 @@ var rpcFunctions = {
 
                             // We check the status
                             // and count till we r done
-                            if(f_length===f_delete_index) {
+                            if (f_length===f_delete_index) {
 
                                 fs.rmdir(path, function(err) {
-                                    if(err) {
+                                    if (err) {
                                         cb(err,null);
                                     }
                                     else {
@@ -290,9 +294,9 @@ var rpcFunctions = {
                             return false;
                         };
 
-                        if(!checkStatus()) {
+                        if (!checkStatus()) {
 
-                            for(var i=0;i<f_length;i++) {
+                            for (var i = 0; i < f_length; i++) {
 
                                 // Create a local scope for filePath
                                 // Not really needed, but just good practice
@@ -301,8 +305,8 @@ var rpcFunctions = {
                                     var filePath = path + vars.globals.fileSeparator + files[i];
                                     // Add a named function as callback
                                     // just to enlighten debugging
-                                    fs.removeRecursive(filePath, function removeRecursiveCB(err,status) {
-                                        if(!err) {
+                                    removeRecursive(filePath, function removeRecursiveCB(err, status) {
+                                        if (!err) {
 
                                             f_delete_index ++;
                                             checkStatus();
@@ -322,15 +326,22 @@ var rpcFunctions = {
             });
         };
 
-        fs.removeRecursive(projectPath, function (error, status){
+        removeRecursive(projectPath, function (error, status){
             if(!error) {
                 console.log("Succesfull deletion of directory " + projectPath);
-                sendResponse({hasError: false, data: "Succesfull deletion of project " + projectName});
+                if (!responseSent) {
+                    sendResponse({hasError: false, data: "Succesfull deletion of project " + projectName});
+                    responseSent = true;
+                }
+                
             }
             else {
                 console.log("Error in deletion of directory " + projectPath);
                 console.log("Error deleting project: " + error);
-                sendResponse({hasError: true, data: "Error deleting project: " + error});
+                if (!responseSent) {
+                    sendResponse({hasError: true, data: "Error deleting project: " + error});    
+                    responseSent = true;
+                }
             }
 
         });

ReloadServer/lib/jsonrpc.js
@@ -28,6 +28,7 @@ var JSONRPC = {
 
 		/*var self = this;
 		this.response = response;*/
+		resObj = response;
 
 		this.handleMessage(message, function(processingResult) {
 			
@@ -141,15 +142,15 @@ var JSONRPC = {
 	        	'error': 'Invalid Request'
 	      	};
 
-	      	this.response.writeHead(200, {
+	      	resObj.writeHead(200, {
 						  'Content-Length': JSON.stringify(responseObject).length,
 						  'Content-Type': 'application/json',
 						  'Pragma': 'no-cache',
 						  'Cache-Control': 'no-cache',
 						  'Expires': '-1'
 						});
-			this.response.write( JSON.stringify(responseObject) );
-			this.response.end("");
+			resObj.write( JSON.stringify(responseObject) );
+			resObj.end("");
 
 	    	return this;
 	    }
@@ -162,15 +163,15 @@ var JSONRPC = {
 	        	'error': 'Function not found'
 	      	};
 
-	      	this.response.writeHead(200, {
+	      	resObj.writeHead(200, {
 						  'Content-Length': JSON.stringify(responseObject).length,
 						  'Content-Type': 'application/json',
 						  'Pragma': 'no-cache',
 						  'Cache-Control': 'no-cache',
 						  'Expires': '-1'
 						});
-			this.response.write( JSON.stringify(responseObject) );
-			this.response.end("");
+			resObj.write( JSON.stringify(responseObject) );
+			resObj.end("");
 
 			return this;
 	    }
@@ -219,15 +220,15 @@ var JSONRPC = {
 		        	'error': 'Invalid Parameters passed to function'
 		      	};
 
-		      	this.response.writeHead(200, {
+		      	resObj.writeHead(200, {
 							  'Content-Length': JSON.stringify(responseObject).length,
 							  'Content-Type': 'application/json',
 							  'Pragma': 'no-cache',
 							  'Cache-Control': 'no-cache',
 							  'Expires': '-1'
 							});
-				this.response.write( JSON.stringify(responseObject) );
-				this.response.end("");
+				resObj.write( JSON.stringify(responseObject) );
+				resObj.end("");
 	    	}
 
 	    }