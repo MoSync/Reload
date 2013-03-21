13871b8415aa0a396daf67b61f4044db27abb0ca
Igor Eklund <igor.eklund@mosync.com>
2013-03-21 15:46:34 +0100

    Include cli in build file

--- build.rb
+++ build.rb
@@ -128,6 +128,9 @@ FileUtils.cp_r "ReloadClient/Clients/Android", "Build/#{time_stamp}/MoSync_Reloa
 FileUtils.cp_r "ReloadClient/Clients/iOS", "Build/#{time_stamp}/MoSync_Reload_Linux/iOS Client"
 FileUtils.cp_r "ReloadClient/Clients/WindowsPhone", "Build/#{time_stamp}/MoSync_Reload_Linux/WindowsPhone Client"
 
+puts "Copying command line tool"
+FileUtils.cp_r "cli", "/Volumes/MoSyncReload/cli"
+
 puts "Copying Readme"
 FileUtils.cp_r "ReadMe.txt", "/Volumes/MoSyncReload/"
 FileUtils.cp_r "ReadMe.txt", "Build/#{time_stamp}/MoSync_Reload_Windows"
